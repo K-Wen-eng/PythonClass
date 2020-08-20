e1867959c608c265f613b852aafdb376d65e4cf9
K-文 <64456864+K-Wen-eng@users.noreply.github.com>
2020-08-20 10:16:48 +0800

    Add files via upload

---
+++ 随机输出手机品牌.py
@@ -0,0 +1,10 @@
+# 请在...处使用一行或多行代码替换
+# 请在______处使用一行代码替换
+#
+# 注意：请不要修改其他已给出代码
+
+import random
+brandlist = ['三星','苹果','vivo','OPPO','魅族']
+random.seed(0)
+name = brandlist[random.randint(0,4)]
+print(name)
